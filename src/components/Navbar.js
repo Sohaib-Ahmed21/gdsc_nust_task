7e6a4544c541ad8399e29a28a7c4e2d5033c1f6f
sohaib <sohaibahmed1919@gmail.com>
2022-10-22 21:56:54 +0500

    Added basic functionality to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../styles/navbar.css"
+import useWindowDimensions from './Dimensions'
 
 export const Navbar = () => {
+	const { height, width } = useWindowDimensions();
+	const isPortrait = (width < 786);
+	const [showHidden, setShowHidden] = useState(false);
+	console.log(width)
+	console.log(showHidden)
 	return (
-		<nav>
-			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Contact Us</li>
-				<li>Log In</li>
-			</ul>
+		<nav className='flex-row space-between items-center'>
+			<div className={isPortrait?'flex-col':'flex-row'}>
+				<div className='nav-item'>Home</div>
+				<div className={isPortrait?'flex-col':'flex-row'}>
+					<div hidden={!showHidden && isPortrait} className='nav-item'>About</div>
+					<div hidden={!showHidden && isPortrait} className='nav-item'>Contact Us</div>
+					<div hidden={!showHidden && isPortrait} className='nav-item'>Log In</div>
+				</div>
+			</div>
+			<div className='nav-btn' onClick={()=>{setShowHidden(!showHidden)}} hidden={!isPortrait}> <img src="/menu.png" alt="" /> </div>
 		</nav>
 	)
 }
